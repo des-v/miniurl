669c6f725eed1222649cf8a325ec21759950eef5
des-v <desireevaldes@gmail.com>
2025-05-21 10:19:03 +0200

    Initial setup

---
+++ src/test/java/com/desireevaldes/miniurl/MiniurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desireevaldes.miniurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
